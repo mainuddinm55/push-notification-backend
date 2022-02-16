19368d0e8eb64e3959d95fe6dbe9323eef3a8408
Md Mainuddin <mainuddinm55@gmail.com>
2022-02-16 15:37:22 +0600

    add event log api.

--- functions/src/controller/event_controller.js
+++ functions/src/controller/event_controller.js
@@ -1,16 +1,7 @@
 exports.logEvent = (req, res, next) => {
     console.log(`DateTime: ${new Date()}`)
     console.log(`EventName: ${req.body.event_name}`)
-    let params = ''
-    let properties = ''
-    Object.keys(req.body.params).forEach(key => {
-        params += `\n${key}: ${req.body.params[key]}`
-    })
-    Object.keys(req.body.user_properties).forEach(key => {
-        params += `\n${key}: ${req.body.user_properties[key]}`
-    })
-
-    console.log(`Params: ${params}`)
-    console.log(`User properties: ${properties}`)
+    console.log(`Params: `+JSON.stringify(req.body.params))
+    console.log(`UserProperties: `+JSON.stringify(req.body.user_properties))
     res.json(req.body)
 }
